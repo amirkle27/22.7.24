315acd3f6ab0a2ad3637a279ca89a988f77127a6
amirkle27 <amirkle@gmail.com>
2024-07-24 01:38:54 +0300

    Update Homework 22.7.24 DataAnalysis no.4.py

--- Homework/Homework 22.7.24 DataAnalysis no.4.py
+++ Homework/Homework 22.7.24 DataAnalysis no.4.py
@@ -1,9 +1,7 @@
 num1: int = int (input("Please enter your first number"));
 num2: int = int (input("Please enter your second number"));
-while num1 != num2:
-    if num1 < num2:
-        num1= num1+1
-    else:
-        num2 = num2 + 1
+ans: int = 0
+for i in range (num1):
+    ans= ans+num2
 print("We just multiplied your two numbers only by addition! Isn't that clever??:")
-print(num1+num2)
+print(ans)
