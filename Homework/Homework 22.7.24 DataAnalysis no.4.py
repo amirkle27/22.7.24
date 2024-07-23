3fd7b97e992741d05e071fa15809f630564fae13
amirkle27 <amirkle@gmail.com>
2024-07-24 01:40:06 +0300

    Update Homework 22.7.24 DataAnalysis no.4.py

--- Homework/Homework 22.7.24 DataAnalysis no.4.py
+++ Homework/Homework 22.7.24 DataAnalysis no.4.py
@@ -2,6 +2,6 @@ num1: int = int (input("Please enter your first number"));
 num2: int = int (input("Please enter your second number"));
 ans: int = 0
 for i in range (num1):
-    ans= ans+num2
+    ans = ans+num2
 print("We just multiplied your two numbers only by addition! Isn't that clever??:")
 print(ans)
