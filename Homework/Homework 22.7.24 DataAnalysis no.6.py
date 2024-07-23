25bbf822ad2be0003b39b6d0afcdf9a49393a519
amirkle27 <amirkle@gmail.com>
2024-07-23 23:44:11 +0300

    Update Homework 22.7.24 DataAnalysis no.6.py

--- Homework/Homework 22.7.24 DataAnalysis no.6.py
+++ Homework/Homework 22.7.24 DataAnalysis no.6.py
@@ -1,5 +1,5 @@
 num: str = str (input("Enter a 3-digit number:"))
-num2: str = str (input("enter another 1 digit number"))
+num2: str = str (input("enter another 1 digit number:"))
 if num2 in num:
      print(True)
 else:
