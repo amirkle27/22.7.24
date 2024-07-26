85bf04191566b75accc301217c5cc6cdaf44a67d
amirkle27 <amirkle@gmail.com>
2024-07-26 22:23:36 +0300

    Update Homework 22.7.24 Conditions no.7.py

--- Homework/Homework 22.7.24 Conditions no.7.py
+++ Homework/Homework 22.7.24 Conditions no.7.py
@@ -1,6 +1,5 @@
-num: int = int(input("Enter a number"));
-num1 = num // 10
-num2 = float ((num/100)*10)
-num3 = (10*(num2-num1))
-ans = (num1+num3)
-print (f"As you first entered the number {num}, the sum of the digits of your number is {ans:.0f}")
+num: int = int(input("Enter a two digits number number"));
+left_d = num // 10
+right_d = num % 10
+sum = (left_d+right_d)
+print (f"As you first entered the number {num}, the sum of the digits of your number is {sum}")
