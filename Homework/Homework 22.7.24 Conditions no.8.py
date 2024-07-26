48284a2a5cd66d1618e345a5d3b2ba66f09a0f9c
amirkle27 <amirkle@gmail.com>
2024-07-26 22:25:42 +0300

    Update Homework 22.7.24 Conditions no.8.py

--- Homework/Homework 22.7.24 Conditions no.8.py
+++ Homework/Homework 22.7.24 Conditions no.8.py
@@ -1,6 +1,4 @@
-num: int = int(input("Enter a number"));
-num1 = num // 10
-num2 = float ((num/100)*10)
-num3 = int (10*(num2-num1))
-num2 = int (num2 //1)
-print (f"As you first entered the number {num}, your new number is {num3}{num2}")
+num: int = int(input("Enter a two digits number"));
+left_d = num // 10
+right_d = num % 10
+print (f"As you first entered the number {num}, your new number is {right_d}{left_d}")
