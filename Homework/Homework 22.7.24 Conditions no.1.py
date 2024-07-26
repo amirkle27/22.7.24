ca2aa05a2d748d61377d21622bc000336c6966b9
amirkle27 <amirkle@gmail.com>
2024-07-26 21:58:33 +0300

    Update Homework 22.7.24 Conditions no.1.py

--- Homework/Homework 22.7.24 Conditions no.1.py
+++ Homework/Homework 22.7.24 Conditions no.1.py
@@ -1,5 +1,5 @@
 min: float = None;
-num: float = float(input("Hello, please enter a decimal numbers"));
+num: float = float(input("Hello, please enter a decimal number"));
 num1: float = float(input("Hello, please enter another decimal numbers"));
 (min := num if num < num1 else (min := num1));
 for j in range (3):
