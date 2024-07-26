10747193591456b86df936cbe435c892758b56fb
amirkle27 <amirkle@gmail.com>
2024-07-26 14:35:24 +0300

    Update Homework 22.7.24 ComplexLoops no.1.py

--- Homework/Homework 22.7.24 ComplexLoops no.1.py
+++ Homework/Homework 22.7.24 ComplexLoops no.1.py
@@ -1,11 +1,17 @@
 import statistics
 temps: list = []
-for i in range(12):
-    temp: int = int(input("What's the average temperature in Tel Aviv this month? "))
-    if -5 <= temp <= 40:
-        temps.append(temp)
-    else:
-        break;
+count: int = 0
+while not temps == (str) and count != 12:
+    temp: float = None
+    try:
+        temp = float(input(f"What's the average temperature in Tel Aviv for this month?"))
+        if -5 <= temp <= 40:
+            temps.append(temp)
+            count +=1
+        else:
+            break;
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}. Please try again:")
 for i in range (len(temps)-1):
     if temps[i] == 0 and temps[i + 1] == 0 and (len(temps)) == 12:
         print(f"There seems to be a mistake, as you entered the temperature 0 twice in a row.")
@@ -14,8 +20,8 @@ for i in range (len(temps)-1):
         new_temp1: int = int (input (f"And the new temperature for month no. {index1}? "))
         index2: int = int (input (f"Please modify your second input. For which month do you want to revise? "))
         new_temp2: int = int (input (f"And the new temperature for month no. {index2}? "))
-        temps [index1 - 1] = new_temp1
-        temps [index2 - 1] = new_temp2
+        temps [index1-1] = new_temp1
+        temps [index2-1] = new_temp2
         print(f"OK, the updated list of annual temperatures for 2023 is: \n{temps}")
 if (len(temps)) == 12:
     print("Correct Data" if -5 < temp < 40 else None)
@@ -23,8 +29,6 @@ if (len(temps)) == 12:
     print(f"The annual average temperature for the year 2023 is {statistics.mean(temps):.2f}")
     print(f"The highest temperature this year was {max(temps)}")
     print(f"The lowest temperature this year was {min(temps)}")
+    #break;
 else:
     print("Wrong Data")
-
-
-
